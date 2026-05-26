64299ebbe8b1d50666591f1ce8ca54947f2358ce
pabltorexx777-gif <parihe75@gmail.com>
2026-05-25 22:29:53 -0600

    fix render port binding

--- backend/index.js
+++ backend/index.js
@@ -258,6 +258,8 @@ app.post('/orders', verificarToken, (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+app.listenconst PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
 });
